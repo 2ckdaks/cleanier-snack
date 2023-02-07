4ee2ef63de923b90c5de942666d0ceeb59c1e96f
이창민 <2ckdaks@gmail.com>
2023-02-07 15:09:39 +0900

    웹안에 링크 글어둠

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extend : true}));
+app.use(express.static("public"));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extend: true }));
 
-app.listen(8080, function(){
-    console.log('listening on 8080')
+app.listen(8080, function () {
+  console.log("listening on 8080");
 });
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/html/index.html");
+});
 
-app.get('/login', function(req, res){
-    res.sendFile(__dirname + '/public/html/login.html')
-})
+app.get("/request", function (req, res) {
+  res.sendFile(__dirname + "/public/html/request.html");
+});
+
+app.get("/snack-list", function (req, res) {
+  res.sendFile(__dirname + "/public/html/snack-list.html");
+});
+
+app.get("/user-list", function (req, res) {
+  res.sendFile(__dirname + "/public/html/user-list.html");
+});
