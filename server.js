ccaf0ac134a2bd5f4fb95eba06ddec6d7608e14a
이창민 <2ckdaks@gmail.com>
2023-02-12 19:32:16 +0900

    ejs

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extend: true }));
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
 var db;
 MongoClient.connect('mongodb+srv://2ckdaks:s3528022@cluster0.0qiowcf.mongodb.net/cleanier-snack?retryWrites=true&w=majority', function(에러, client){
   if (에러) return console.log(에러);
   db = client.db('cleanier-snack');
 
-  db.collection('test').insertOne({이름 : '이창민', 나이 : '26'}, function(req, res){
-    console.log('good')
-  });
+  // db.collection('test').insertOne({이름 : '이창민', 나이 : '26'}, function(req, res){
+  //   console.log('good')
+  // });
 
   //서버띄우는 코드 여기로 옮기기
   app.listen('8080', function(){
@@ -22,30 +22,26 @@ MongoClient.connect('mongodb+srv://2ckdaks:s3528022@cluster0.0qiowcf.mongodb.net
 })
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/public/html/index.html");
+  res.render('index.ejs')
 });
 
 app.get("/login", function (req, res) {
-  res.sendFile(__dirname + "/public/html/login.html");
+  res.render('login.ejs');
 });
 
 app.get("/request", function (req, res) {
-  res.sendFile(__dirname + "/public/html/request.html");
+  res.render('request.ejs');
 });
 app.post('/add', function(req, res){
   console.log(req.body.request);
   res.redirect('/request')
 });
 
-app.post("/add", function(req, res) {
-  res.send('전송완료')
-})
-
 app.get("/snack-list", function (req, res) {
-  res.sendFile(__dirname + "/public/html/snack-list.html");
+  res.render('snack-list.ejs');
 });
 
 app.get("/user-list", function (req, res) {
-  res.sendFile(__dirname + "/public/html/user-list.html");
+  res.render('user-list.ejs');
 });
 
