f5171ef8829a916c9f7b48ccab6a05671225e9a4
이창민 <2ckdaks@gmail.com>
2023-02-17 17:34:38 +0900

    상세정보

--- server.js
+++ server.js
@@ -23,10 +23,6 @@ MongoClient.connect(
     if (에러) return console.log(에러);
     db = client.db("cleanier-snack");
 
-    // db.collection('test').insertOne({이름 : '이창민', 나이 : '26'}, function(req, res){
-    //   console.log('good')
-    // });
-
     //서버띄우는 코드 여기로 옮기기
     app.listen("8080", function () {
       console.log("listening on 8080");
@@ -37,42 +33,6 @@ MongoClient.connect(
 //메인 페이지
 app.get("/", function (req, res) {
   res.render("index.ejs");
-});
-
-//업체등록
-app.get("/sign-up", function (req, res) {
-  res.render("sign-up.ejs");
-});
-app.post("/sign-up", function (req, res) {
-  db.collection("user-login").insertOne(
-    { name: req.body.user_name, id: req.body.user_id, pw: req.body.user_pw },
-    function (에러, 결과) {
-      console.log("업체등록 완료");
-      res.redirect("/admin-user-list");
-    }
-  );
-});
-
-// 관리자 페이지
-
-//간식관리 페이지
-app.get("/admin-snack-list", function (req, res) {
-  db.collection("test")
-    .find()
-    .toArray(function (에러, 결과) {
-      console.log(결과);
-      res.render("admin-snack-list.ejs", { test: 결과 });
-    });
-});
-
-//고객관리 페이지
-app.get("/admin-user-list", function (req, res) {
-  db.collection("user-login")
-    .find()
-    .toArray(function (에러, 결과) {
-      console.log(결과);
-      res.render("admin-user-list.ejs", { userList: 결과 });
-    });
 });
 
 //로그인 페이지
@@ -135,25 +95,22 @@ passport.deserializeUser(function (아이디, done) {
   });
 });
 
-//~경로로 요청을했을때 미들함수 저 로그인했니를 거치면
-// app.get("/request", 로그인했니, function (req, res) {
-//   db.collection("post")
-//     .find()
-//     .toArray(function (에러, 결과) {
-//       console.log(결과);
-//       res.render("request.ejs", { posts: 결과, 사용자: req.user });
-//     });
-// });
+function 로그인했니(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
 
 //요청사항 리스트
 app.get("/request", 로그인했니, function (req, res) {
-  db.collection("test")
+  db.collection("user-request")
     .find({ writer: req.user._id })
     .toArray(function (에러, 결과) {
       console.log(결과);
       res.render("request.ejs", {
         test: 결과,
-        posts: 결과,
         사용자: req.user,
         writer: req.user._id,
       });
@@ -162,11 +119,11 @@ app.get("/request", 로그인했니, function (req, res) {
 
 //요청사항 전송
 app.post("/add-request", 로그인했니, function (req, res) {
-  db.collection("request-counter").findOne(
-    { name: "게시물갯수" },
+  db.collection("counter").findOne(
+    { name: "request-counter" },
     function (에러, 결과) {
       var 총게시물갯수 = 결과.totalRequest;
-      db.collection("test").insertOne(
+      db.collection("user-request").insertOne(
         {
           _id: 총게시물갯수 + 1,
           request: req.body.request,
@@ -174,8 +131,8 @@ app.post("/add-request", 로그인했니, function (req, res) {
         },
         function (에러, 결과) {
           console.log("요청사항 전송 완료");
-          db.collection("request-counter").updateOne(
-            { name: "게시물갯수" },
+          db.collection("counter").updateOne(
+            { name: "request-counter" },
             { $inc: { totalRequest: 1 } },
             function (에러, 결과) {
               if (에러) {
@@ -194,21 +151,26 @@ app.post("/add-request", 로그인했니, function (req, res) {
 //요청사항 삭제
 app.delete("/delete-request", function (req, res) {
   req.body._id = parseInt(req.body._id);
-  db.collection("test").deleteOne(req.body, function (에러, 결과) {
+  db.collection("user-request").deleteOne(req.body, function (에러, 결과) {
     console.log("삭제완료");
     res.redirect("/request");
   });
 });
 
-function 로그인했니(req, res, next) {
-  if (req.user) {
-    next();
-  } else {
-    res.redirect("/login");
-  }
-}
+//간식목록 설정
+app.get("/add-snack2", function (req, res) {
+  res.render("add-snack.ejs");
+});
+app.post("/add", function (req, res) {
+  db.collection("test2").updateOne(
+    { _id: 1 },
+    { $push: { snack: { img: "img-1", snack: "snack-1" } } }
+  );
+  res.redirect("/add-snack2");
+});
 
-app.get("/logout", function (req, res, next) {
+//로그아웃
+app.get("/logout", 로그인했니, function (req, res, next) {
   req.logout(function (err) {
     if (err) {
       console.log(err);
@@ -216,4 +178,71 @@ app.get("/logout", function (req, res, next) {
     }
     res.redirect("/login");
   });
+});
+
+//간식관리
+app.get("/admin-snack-list", function (req, res) {
+  db.collection("test")
+    .find()
+    .toArray(function (에러, 결과) {
+      console.log(결과);
+      res.render("admin-snack-list.ejs", { test: 결과 });
+    });
+});
+
+//고객관리 페이지
+app.get("/admin-user-list", function (req, res) {
+  db.collection("user-login")
+    .find()
+    .toArray(function (에러, 결과) {
+      console.log(결과);
+      res.render("admin-user-list.ejs", { userList: 결과 });
+    });
+});
+
+//고객상세 페이지
+app.get("/admin-user-detail/:id", async function async(req, res) {
+  const 업체명 = await db
+    .collection("user-login")
+    .findOne({ _id: parseInt(req.params.id) });
+  const 요청사항 = await db
+    .collection("user-request")
+    .find({ writer: parseInt(req.params.id) })
+    .toArray();
+  res.render("admin-user-detail.ejs", { 업체명, 요청사항 });
+});
+
+//업체등록
+app.get("/sign-up", function (req, res) {
+  res.render("sign-up.ejs");
+});
+app.post("/sign-up", function (req, res) {
+  db.collection("counter").findOne(
+    { name: "user-counter" },
+    function (에러, 결과) {
+      var 총게시물갯수 = 결과.totalUser;
+      db.collection("user-login").insertOne(
+        {
+          _id: 총게시물갯수 + 1,
+          name: req.body.user_name,
+          id: req.body.user_id,
+          pw: req.body.user_pw,
+        },
+        function (에러, 결과) {
+          console.log("업체등록 완료");
+          db.collection("counter").updateOne(
+            { name: "user-counter" },
+            { $inc: { totalUser: 1 } },
+            function (에러, 결과) {
+              if (에러) {
+                return console.log(에러);
+              }
+              console.log("업체카운팅 +1 완료");
+              res.redirect("/admin-user-list");
+            }
+          );
+        }
+      );
+    }
+  );
 });
